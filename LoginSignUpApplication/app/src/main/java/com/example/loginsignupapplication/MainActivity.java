31b3a908a25f4804d96734a95cdaaac0f45d45db
kerathy <xxyxuxiaoyan@126.com>
2021-06-05 20:19:31 -0600

    login and sign up application

---
+++ LoginSignUpApplication/app/src/main/java/com/example/loginsignupapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.loginsignupapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView2);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
