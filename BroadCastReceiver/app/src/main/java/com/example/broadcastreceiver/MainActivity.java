09562b2227cb517827b4b789a3bf9469ee87e468
kerathy <xxyxuxiaoyan@126.com>
2021-06-09 08:26:12 -0600

    local broadcast manager practice

---
+++ BroadCastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.broadcastreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button button;
+    private TextView textView;
+    private MyBroadcastReceiver myBroadcastReceiver;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button);
+        textView = findViewById(R.id.textView2);
+
+        myBroadcastReceiver = new MyBroadcastReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("YOUR_ACTION");
+        LocalBroadcastManager.getInstance(this).registerReceiver(myBroadcastReceiver, intentFilter);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BroadCast.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private class MyBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals("YOUR_ACTION")) {
+                String data = intent.getStringExtra("DATA");
+                textView.setText(data);
+                Log.d("You received data: ", data);
+            } else {
+                Toast.makeText(context, "Action Not Found", Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(myBroadcastReceiver);
+    }
+}
---
+++ local-broadcast-manager/app/src/main/java/com/example/broadcastreceiver/BroadCast.java
@@ -0,0 +1,40 @@
+package com.example.broadcastreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class BroadCast extends AppCompatActivity {
+
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broad_cast);
+
+        button = findViewById(R.id.sendMessage);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("YOUR_ACTION")
+                        .putExtra("DATA", "MindOrks -----");
+                LocalBroadcastManager.getInstance(BroadCast.this).sendBroadcast(intent);
+            }
+        });
+    }
+
+
+}
