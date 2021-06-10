36d5bd4457013d5983ef0f39c1f3637dad597b73
kerathy <xxyxuxiaoyan@126.com>
2021-06-10 14:53:49 -0600

    update Question1

--- kotlin-practice0610/src/Question1/Main.kt
+++ kotlin-practice0610/src/Question1/Main.kt
@@ -1,8 +1,37 @@
 package Question1
 
-fun main() {
-    val myBook = MyBook("The Alchemist", "Paulo Coelho", 248)
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val title = scanner.nextLine()
+    val author = scanner.nextLine()
+    val price = Integer.valueOf(scanner.nextLine())
+
+    val myBook = MyBook(title, author, price)
     myBook.display()
 }
 
 
+abstract class Book {
+    var title: String = ""
+    var author: String = ""
+    var price = 0
+
+    constructor(_title: String, _author: String, _price: Int) {
+        title = _title
+        author = _author
+        price = _price
+    }
+    abstract fun display()
+}
+
+class MyBook: Book {
+    constructor(_title: String, _author: String, _price: Int) : super(_title, _author, _price)
+
+    override fun display() {
+        println("Title: " + this.title)
+        println("Author: " + this.author)
+        println("Price: " + this.price)
+    }
+}
