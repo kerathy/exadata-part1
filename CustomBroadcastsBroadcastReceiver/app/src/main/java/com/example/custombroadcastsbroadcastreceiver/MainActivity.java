81cd84f9750766643cf536f6fd967a299ec2ad28
kerathy <xxyxuxiaoyan@126.com>
2021-06-15 00:05:37 -0600

    update part3 custom broadcast, broadcast receiving app

--- CustomBroadcastsBroadcastReceiver/app/src/main/java/com/example/custombroadcastsbroadcastreceiver/MainActivity.java
+++ CustomBroadcastsBroadcastReceiver/app/src/main/java/com/example/custombroadcastsbroadcastreceiver/MainActivity.java
@@ -1,3 +1,5 @@
+
+
 package com.example.custombroadcastsbroadcastreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,24 +9,45 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     ExampleBroadcastReceiver exampleBroadcastReceiver = new ExampleBroadcastReceiver();
+    static final String TAG = "MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate method is invoked");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void sendBroadcast(View v) {
+        Intent intent = new Intent("ACTION");
+        intent.putExtra("TEXT", "Broadcast received");
+        sendBroadcast(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i(TAG, "on Start method is invoked");
+        super.onStart();
         IntentFilter intentFilter = new IntentFilter("ACTION");
-        System.out.println("intentfilter: " + intentFilter);
         registerReceiver(exampleBroadcastReceiver, intentFilter);
     }
 
     @Override
+    protected void onStop() {
+        Log.i(TAG, "on Stop method is invoked");
+        super.onStop();
+    }
+
+    @Override
     protected void onDestroy() {
+        Log.i(TAG, "on Destroy method is invoked");
         super.onDestroy();
         unregisterReceiver(exampleBroadcastReceiver);
     }
@@ -34,8 +57,10 @@ public class MainActivity extends AppCompatActivity {
 class ExampleBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.i("MESSAGE", "onReceive method is invoked");
         if ("ACTION".equals(intent.getAction())) {
             String text = intent.getStringExtra("TEXT");
+            System.out.println("text: Receive the content of TEXT" + text);
             Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
         }
     }
