16f8195cdef8e212574047f253267158110b7448
kerathy <xxyxuxiaoyan@126.com>
2021-06-28 20:45:34 -0600

    material card view

--- material-card-view/app/src/main/java/com/example/materialcardviewpractice/MaterialCardViewWithPadding.java
+++ material-card-view/app/src/main/java/com/example/materialcardviewpractice/MaterialCardViewWithPadding.java
@@ -2,7 +2,9 @@ package com.example.materialcardviewpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MaterialCardViewWithPadding extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MaterialCardViewWithPadding extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material_card_view_with_padding);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MaterialCardViewWithPadding.this, MaterialCardViewWithoutPadding.class));
+            }
+        }, 1000);
     }
 }
--- material-card-view/app/src/main/java/com/example/materialcardviewpractice/MaterialCardViewWithoutPadding.java
+++ material-card-view/app/src/main/java/com/example/materialcardviewpractice/MaterialCardViewWithoutPadding.java
@@ -12,11 +12,6 @@ public class MaterialCardViewWithoutPadding extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material_card_view_without_padding);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(MaterialCardViewWithoutPadding.this, MaterialCardViewWithPadding.class));
-            }
-        }, 1000);
+
     }
 }
