ce4a41774f87fc25fd8fff4ee46e7fa0cdf2426a
kerathy <xxyxuxiaoyan@126.com>
2021-06-30 18:53:14 -0600

    update

--- material-card-view/app/src/main/java/com/example/materialcardviewpractice/MaterialCardViewWithPadding2.java
+++ material-card-view/app/src/main/java/com/example/materialcardviewpractice/MaterialCardViewWithPadding2.java
@@ -2,13 +2,91 @@ package com.example.materialcardviewpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MaterialCardViewWithPadding2 extends AppCompatActivity {
+import java.lang.reflect.Array;
+
+public class MaterialCardViewWithPadding2 extends AppCompatActivity{
+
+    String selectedTitle;
+    String selectedGender;
+    String selectedCountry;
+    RadioGroup radioGroup;
+    RadioButton radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material_card_view_with_padding2);
+
+        Spinner titleDropDown = findViewById(R.id.title_spinner);
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.titles, android.R.layout.simple_spinner_item);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        titleDropDown.setAdapter(arrayAdapter);
+        titleDropDown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedTitle = parent.getItemAtPosition(position).toString();
+                System.out.println("title: " + selectedTitle);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        Spinner genderDropDown = findViewById(R.id.spinner_gender);
+        ArrayAdapter<CharSequence> genderAdapter = ArrayAdapter.createFromResource(this, R.array.gender, android.R.layout.simple_spinner_item);
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderDropDown.setAdapter(genderAdapter);
+        genderDropDown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedGender = parent.getItemAtPosition(position).toString();
+                System.out.println("gender: " + selectedGender);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        Spinner countryDropDown = findViewById(R.id.spinner_country);
+        ArrayAdapter<CharSequence> countryAdapter = ArrayAdapter.createFromResource(this, R.array.country, android.R.layout.simple_spinner_item);
+        countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        countryDropDown.setAdapter(countryAdapter);
+        countryDropDown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedCountry = parent.getItemAtPosition(position).toString();
+                System.out.println("country: " + selectedCountry);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        radioGroup = findViewById(R.id.radio_group);
     }
+
+    public void checkRadio(View view) {
+        int radioId = radioGroup.getCheckedRadioButtonId();
+        radioButton = findViewById(radioId);
+        System.out.println("radioButton: " + radioButton.getText());
+    }
+
 }
