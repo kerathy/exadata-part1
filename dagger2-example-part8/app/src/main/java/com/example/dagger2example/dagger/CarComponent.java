fa546b9f75d316a4c090fe340280234d7697628b
kerathy <xxyxuxiaoyan@126.com>
2021-07-08 13:51:54 -0600

    component.builder, bindsinstance, nammed

---
+++ dagger2-example-part8/app/src/main/java/com/example/dagger2example/dagger/CarComponent.java
@@ -0,0 +1,56 @@
+package com.example.dagger2example.dagger;
+
+import com.example.dagger2example.car.Car;
+import com.example.dagger2example.MainActivity;
+import com.example.dagger2example.car.PetrolEngine;
+
+import javax.inject.Named;
+
+import dagger.BindsInstance;
+import dagger.Component;
+
+//@Component
+//public interface CarComponent {
+//
+//    Car getCar();
+//}
+
+
+
+//field injection
+
+//@Component (modules = {WheelsModule.class, PetrolEngineModule.class})
+//public interface CarComponent {
+//
+//    Car getCar();
+//
+//    void inject(MainActivity mainActivity);
+//}
+
+//@Component (modules = {WheelsModule.class, DieselEngineModule.class})
+//public interface CarComponent {
+//
+//    Car getCar();
+//
+//    void inject(MainActivity mainActivity);
+//}
+
+@Component (modules = {WheelsModule.class, PetrolEngineModule.class})
+public interface CarComponent {
+
+    Car getCar();
+
+    void inject(MainActivity mainActivity);
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        Builder horsePower(@Named("horse power") int horsePower);
+
+        @BindsInstance
+        Builder engineCapacity(@Named("engine capacity") int engineCapacity);
+
+        CarComponent build();
+    }
+}
