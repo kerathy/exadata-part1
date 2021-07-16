11252c0a1a84b67d9633ec4bfb38bbc3f7844c34
kerathy <xxyxuxiaoyan@126.com>
2021-07-16 13:05:01 -0600

    dagger2 part2

---
+++ practice3/app/src/main/java/com/example/practice3/WarriorPresenterActivity.java
@@ -0,0 +1,14 @@
+package com.example.practice3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WarriorPresenterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_warrior_presenter);
+    }
+}
