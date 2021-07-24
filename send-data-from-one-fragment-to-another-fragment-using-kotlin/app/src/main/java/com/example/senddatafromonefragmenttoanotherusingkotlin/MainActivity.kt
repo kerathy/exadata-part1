3973b8bd3a1dda559af86e09c667de1627aab245
kerathy <xxyxuxiaoyan@126.com>
2021-07-24 13:59:50 -0600

    update

--- send-data-from-one-fragment-to-another-fragment-using-kotlin/app/src/main/java/com/example/senddatafromonefragmenttoanotherusingkotlin/MainActivity.kt
+++ send-data-from-one-fragment-to-another-fragment-using-kotlin/app/src/main/java/com/example/senddatafromonefragmenttoanotherusingkotlin/MainActivity.kt
@@ -1,15 +1,5 @@
 package com.example.senddatafromonefragmenttoanotherusingkotlin
 
-//import androidx.appcompat.app.AppCompatActivity
-//import android.os.Bundle
-//
-//class MainActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//    }
-//}
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentTransaction
