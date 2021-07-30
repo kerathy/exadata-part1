d60044c5fd051d996805fca92eb1f9740416a2d2
kerathy <xxyxuxiaoyan@126.com>
2021-07-29 22:40:29 -0600

    fragment communication using viewmodel

---
+++ fragment-communication-using-viewmodel/app/src/main/java/com/example/fragmentcommunicationusingviewmodel/SecondFragment.java
@@ -0,0 +1,60 @@
+package com.example.fragmentcommunicationusingviewmodel;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SecondFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SecondFragment extends Fragment {
+
+    private PageViewModel pageViewModel;
+    private TextView txtName;
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+    public static SecondFragment newInstance() {
+        return new SecondFragment();
+    }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // initialise ViewModel here
+        pageViewModel = ViewModelProviders.of(requireActivity()).get(PageViewModel.class);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+
+    @Override public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        txtName = view.findViewById(R.id.textViewName);
+        pageViewModel.getName().observe(requireActivity(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                txtName.setText(s);
+            }
+        });
+    }
+
+
+}
