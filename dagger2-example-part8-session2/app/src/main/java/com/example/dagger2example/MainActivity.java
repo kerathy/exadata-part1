7868638a3af64ac3f0b797ef2d41378ae74ee468
kerathy <xxyxuxiaoyan@126.com>
2021-08-05 21:03:54 -0600

    dagger2 example part8 session2

---
+++ dagger2-example-part8-session2/app/src/main/java/com/example/dagger2example/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.dagger2example;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.dagger2example.car.Car;
+import com.example.dagger2example.dagger.CarComponent;
+import com.example.dagger2example.dagger.DaggerCarComponent;
+import com.example.dagger2example.dagger.DieselEngineModule;
+
+import javax.inject.Inject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    Car car;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CarComponent carComponent = DaggerCarComponent.builder()
+                .horsePower(150)
+                .engineCapacity(1400)
+                .build();
+        carComponent.inject(this);
+        car.drive();
+    }
+}
---
+++ dagger2-example-part8-session2/app/src/main/java/com/example/dagger2example/car/Car.java
@@ -0,0 +1,29 @@
+package com.example.dagger2example.car;
+
+import android.util.Log;
+import javax.inject.Inject;
+
+
+public class Car {
+
+    private static final String TAG = "Print: Car";
+
+    @Inject
+    Engine engine;
+    private Wheels wheels;
+
+    @Inject
+    public Car(Wheels wheels) {
+        this.wheels = wheels;
+    }
+
+    public void drive() {
+        engine.start();
+        Log.d(TAG, "drive method...");
+    }
+
+    @Inject
+    public void enableRemote(Remote remote) {
+        remote.setListener(this);
+    }
+}
