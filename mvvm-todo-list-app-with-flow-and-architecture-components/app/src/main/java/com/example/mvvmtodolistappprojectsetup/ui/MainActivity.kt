bbd4953aab5118513a56e6bdf47d70a83d4da594
kerathy <xxyxuxiaoyan@126.com>
2021-08-07 14:06:24 -0600

    navigation component

---
+++ mvvm-todo-list-app-with-flow-and-architecture-components/app/src/main/java/com/example/mvvmtodolistappprojectsetup/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mvvmtodolistappprojectsetup.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvvmtodolistappprojectsetup.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
