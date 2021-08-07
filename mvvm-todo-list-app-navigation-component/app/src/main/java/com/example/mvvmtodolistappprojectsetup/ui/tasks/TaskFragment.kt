7975d411b3f7d47e91b8d4fcd6e801fafbf24948
kerathy <xxyxuxiaoyan@126.com>
2021-08-07 14:10:27 -0600

    navigation component

---
+++ mvvm-todo-list-app-navigation-component/app/src/main/java/com/example/mvvmtodolistappprojectsetup/ui/tasks/TaskFragment.kt
@@ -0,0 +1,7 @@
+package com.example.mvvmtodolistappprojectsetup.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.example.mvvmtodolistappprojectsetup.R
+
+class TaskFragment : Fragment(R.layout.fragment_tasks) {
+}
