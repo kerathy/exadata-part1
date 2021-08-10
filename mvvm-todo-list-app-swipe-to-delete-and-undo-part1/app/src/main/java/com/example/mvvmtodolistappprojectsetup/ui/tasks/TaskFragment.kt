66f858b7627af5ad032724dfa389878301b63ff4
kerathy <xxyxuxiaoyan@126.com>
2021-08-10 14:38:39 -0600

    swipe to delete and undo part1

---
+++ mvvm-todo-list-app-swipe-to-delete-and-undo-part1/app/src/main/java/com/example/mvvmtodolistappprojectsetup/ui/tasks/TaskFragment.kt
@@ -0,0 +1,112 @@
+package com.example.mvvmtodolistappprojectsetup.ui.tasks
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mvvmtodolistappprojectsetup.R
+import com.example.mvvmtodolistappprojectsetup.data.SortOrder
+import com.example.mvvmtodolistappprojectsetup.data.Task
+import com.example.mvvmtodolistappprojectsetup.databinding.FragmentTasksBinding
+import com.example.mvvmtodolistappprojectsetup.ui.util.onQueryTextChanged
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class TaskFragment : Fragment(R.layout.fragment_tasks), TaskAdapter.OnItemClickListener {
+
+    private val viewModel: TaskViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTasksBinding.bind(view)
+
+        val taskAdapter = TaskAdapter(this)
+
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = taskAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+
+            ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    return false
+                }
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    val task = taskAdapter.currentList[viewHolder.adapterPosition]
+                    viewModel.onTaskSwiped(task)
+                }
+            }).attachToRecyclerView(recyclerViewTasks)
+        }
+
+        viewModel.tasks.observe(viewLifecycleOwner) {
+            taskAdapter.submitList(it)
+        }
+        setHasOptionsMenu(true)
+    }
+
+    override fun onItemClick(task: Task) {
+        viewModel.onTaskSelected(task)
+
+    }
+
+    override fun onCheckBoxClick(task: Task, isChecked: Boolean) {
+       viewModel.onTaskCheckedChanged(task, isChecked);
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_tasks, menu);
+
+        val searchItem = menu.findItem(R.id.action_search);
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            viewModel.searchQuery.value = it
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            menu.findItem(R.id.action_hide_completed_tasks).isChecked =
+                viewModel.preferencesFlow.first().hideCompleted
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_sort_by_name -> {
+                viewModel.onSortOrderSelected(SortOrder.BY_NAME)
+                true
+            }
+            R.id.action_sort_by_date_created -> {
+                viewModel.onSortOrderSelected(SortOrder.BY_DATE)
+                true
+            }
+            R.id.action_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+                viewModel.onHideCompletedClick(item.isChecked)
+                true
+            }
+            R.id.action_delete_all_completed_tasks -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
---
+++ mvvm-todo-list-app-swipe-to-delete-and-undo-part1/app/src/main/java/com/example/mvvmtodolistappprojectsetup/ui/tasks/TaskViewModel.kt
@@ -0,0 +1,63 @@
+package com.example.mvvmtodolistappprojectsetup.ui.tasks
+
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.mvvmtodolistappprojectsetup.data.PreferenceManager
+import com.example.mvvmtodolistappprojectsetup.data.SortOrder
+import com.example.mvvmtodolistappprojectsetup.data.Task
+import com.example.mvvmtodolistappprojectsetup.data.TaskDao
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class TaskViewModel @Inject constructor(
+    private val taskDao: TaskDao,
+    private val preferencesManager: PreferenceManager
+
+) : ViewModel(), LifecycleObserver {
+    val searchQuery = MutableStateFlow("")
+
+    val preferencesFlow = preferencesManager.preferencesFlow
+
+    private val tasksFlow = combine(
+        searchQuery,
+        preferencesFlow
+    ) { query, filterPreferences ->
+        Pair(query, filterPreferences)
+    }.flatMapLatest { (query, filterPreferences) ->
+        taskDao.getTasks(query, filterPreferences.sortOrder, filterPreferences.hideCompleted)
+    }
+
+    fun onSortOrderSelected(sortOrder: SortOrder) = viewModelScope.launch {
+        preferencesManager.updateSortOrder(sortOrder)
+    }
+
+    fun onHideCompletedClick(hideCompleted: Boolean) = viewModelScope.launch {
+        preferencesManager.updateHideCompleted(hideCompleted)
+    }
+//        searchQuery.flatMapLatest {
+//        taskDao.getTasks(it)
+
+    //    val tasks = taskDao.getTasks("bia").asLiveData()
+    val tasks = tasksFlow.asLiveData()
+
+    fun onTaskSelected(task: Task) {
+
+    }
+
+    fun onTaskCheckedChanged(task: Task, isChecked: Boolean) = viewModelScope.launch {
+        taskDao.update(task.copy(completed = isChecked))
+    }
+
+    fun onTaskSwiped(task: Task) = viewModelScope.launch {
+        taskDao.delete(task);
+    }
+}
+
+
